62981b951817c01b608bd3f48902402d9527db48
m.salman <m.salman@tpsonline.com>
2022-05-20 16:29:33 +0500

    key changes

--- lib/Utils/stripe_keys.dart
+++ lib/Utils/stripe_keys.dart
@@ -1,9 +1,3 @@
-// const String publishableKey = 'your publishableKey';
-//
-// const String secretKey = 'your secretKey';
+const String publishableKey = 'your publishableKey';
 
-const String publishableKey =
-    'pk_test_51L1SmQJuyWXj3bGWLKK0DPi0aV5eb5hltZLuiwTkEBUz1zhnV08AidgIMHDW13Q3JhewkzxWc48X16msLUwb2aJY00SwymmvtE';
-
-const String secretKey =
-    'sk_test_51L1SmQJuyWXj3bGW39oD2oos9JS4P87a4f7PCknJynF12igCaG0fQycKBevjxl1gX0cOC4FJUBIO3swKxFS2tzR900Y4xQkGhm';
+const String secretKey = 'your secretKey';
